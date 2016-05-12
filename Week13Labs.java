707822ad6cf2d5095e0e734b15c432e3049ad496
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-05-11 20:56:53 -0700

    Add files via upload

---
+++ Week13Labs.java
@@ -0,0 +1,11 @@
+package week13labs;
+
+public class Week13Labs {
+
+    public static void main(String[] args) {
+        ThreeDice test = new ThreeDice();
+        System.out.println("Average of three dice is: " + test.Throw());
+        
+    }
+    
+}
